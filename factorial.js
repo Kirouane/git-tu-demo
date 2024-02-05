18c77181f680d27389dfd36d62955baee1b6ad29
Nassim Kirouane <nkirouane@securkeys.com>
2024-02-05 10:07:58 +0100

    3e commit

--- factorial.js
+++ factorial.js
@@ -1,12 +1,7 @@
 function factorial(n) {
-  if (n === 0) {
-    return 1;
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
   }
-
-  let resultat = 1;
-  for (let i = 1; i <= n; i++) {
-    resultat *= i;
-  }
-
-  return resultat;
+  return result;
 }
